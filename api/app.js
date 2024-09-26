09697af96e06e4def43dfcb070050b8e515c3f9f
Mathesh Ramesh <ramesh.mat@northeastern.edu>
2024-09-26 13:31:51 -0400

    updated payload check

--- api/app.js
+++ api/app.js
@@ -18,8 +18,7 @@ const db = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env
 const initialize = (app) => {
   app.use(express.json());
   app.get("/healthz", (req, res) => {
-
-    if (Object.keys(req.query).length !== 0) {
+    if (Object.keys(req.query).length !== 0 || (req.body && Object.keys(req.body).length !== 0)) {
       return res.status(400).send();
     }
 
