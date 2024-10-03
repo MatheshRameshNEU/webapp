7f944a3b8674d3122d6aba0130511d971c960e68
Mathesh Ramesh <ramesh.mat@northeastern.edu>
2024-10-03 10:36:55 -0400

    created user api and db

---
+++ api/models/user.js
@@ -0,0 +1,52 @@
+// models/user.js
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    account_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    account_updated: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  }, {
+    timestamps: false 
+  });
+
+  // Automatically update 'account_updated' when a record is modified
+  User.beforeUpdate((user) => {
+    user.account_updated = new Date();
+  });
+
+  return User;
+};
