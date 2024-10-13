8786a19d66ca87b38f87928f123b4cf3561d2af4
Mathesh Ramesh <ramesh.mat@northeastern.edu>
2024-10-13 19:24:36 -0400

    update packer build

--- packer/install.sh
+++ packer/install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-# Update package lists
+
 sudo apt update
 
-# Install PostgreSQL and required packages
 sudo apt install -y postgresql postgresql-contrib
 
-# Install Node.js
+
 sudo apt install -y nodejs
 
-# Install npm (Node Package Manager)
+
 sudo apt install -y npm
 
-# Switch to the postgres user to configure PostgreSQL
 sudo -i -u postgres bash << EOF
 
 # Access the PostgreSQL interactive terminal
@@ -20,10 +18,6 @@ psql -c "ALTER USER postgres WITH PASSWORD 'Welcome@1';"
 
 EOF
 
-# Enable PostgreSQL service to start on boot
-sudo systemctl enable postgresql
 
-# Verify installations
-node -v  # Check Node.js version
-npm -v   # Check npm version
-psql --version  # Check PostgreSQL version
+sudo systemctl enable postgresql
+n
