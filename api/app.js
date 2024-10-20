b69a48c9e2c6f539caa50a7859168715cf9ebee4
Mathesh Ramesh <ramesh.mat@northeastern.edu>
2024-10-20 13:40:04 -0400

    remove db setup and updated db connection

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ const db = new Sequelize(
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
-    host: "process.env.DB_HOST" || localhost,
+    host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || 5432,
     dialect: "postgres",
   }
